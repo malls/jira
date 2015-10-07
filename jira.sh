12d6d132cd762908bfdab36f749d703929828564
Forrest Almasi <forrest.almasi@gmail.com>
2015-10-07 11:10:18 -0400

    master and live handling

--- jira.sh
+++ jira.sh
@@ -1,19 +1,16 @@
-
-base_url='https://businessinsider.atlassian.net/browse/'
-
-echo $base_url
-
-branch=$(command git rev-parse --abbrev-ref HEAD)
-
-echo $branch
-
-full_url=${base_url}${branch}
-
 for opener in xdg-open open cygstart "start"; {
-  if command -v $opener; then
-    open=$opener;
-    break;
-  fi
+ 	if command -v $opener; then
+    	open=$opener;
+    	break;
+  	fi
 }
 
-$open $full_url
+base_url='https://businessinsider.atlassian.net/browse/'
+branch=$(command git rev-parse --abbrev-ref HEAD)
+full_url=${base_url}${branch}
+
+if [ $branch == 'master' ] || [ $branch == 'live' ]; then
+	$open $base_url
+else 
+	$open $full_url
+fi
