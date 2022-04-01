4a815c815a1ce7ccabbd1315baacc7e5de91c4bc
Forrest Almasi <forrest@MacBook-Pro-3.localdomain>
2022-04-01 12:33:35 -0400

    have script use environment variable, and no explicit non-ticket branches

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Browser opener for tickets in Jira. If your branch name matches your ticket name, `jira` in your terminal will open the ticket in your browser.
 
-`chmod +x jira.sh && cp jira.sh /usr/local/bin/jira`
+In your `.zshrc` or whereever, set an environment variable `export JIRA_DOMAIN="whateversubdomain"`.
+
+Then install like:
+
+`$ chmod +x jira.sh && cp jira.sh /usr/local/bin/jira`
--- jira.sh
+++ jira.sh
@@ -7,12 +7,16 @@ for opener in xdg-open open cygstart "start"; {
   	fi
 }
 
-base_url='https://compa55.atlassian.net/browse/'
-branch=$(command git rev-parse --abbrev-ref HEAD)
-full_url=${base_url}${branch}
+protocol='https://';
+base_url='.atlassian.net/browse/';
+branch=$(command git rev-parse --abbrev-ref HEAD);
 
-if [ $branch == 'master' ] || [ $branch == 'live' ]; then
-	$open $base_url
-else 
-	$open $full_url
+# See if branch has a number in it. If so, we assume it's a ticket
+if echo "${branch}" | grep '[0-9]' >/dev/null; then
+	ticket=$branch
 fi
+
+# Set JIRA_DOMAIN as env var, see README.md
+full_url=${protocol}${JIRA_DOMAIN}${base_url}${ticket};
+
+$open $full_url;
